67ee14ec6e4c4afbf6bab2d10f8325c1e468193a
Lyn Headley <laheadle@gmail.com>
2010-03-01 16:07:53 -0800

    add Name enum top-level class

---
+++ project/src/org/rapidnewsawards/shared/Name.java
@@ -0,0 +1,16 @@
+package org.rapidnewsawards.shared;
+
+public enum Name {
+
+	JOURNALISM("Journalism"),
+	USERNAME("username"), 
+	URL("url"), 
+	NAME("name");
+
+	public String name;
+
+	Name(String name) {
+		this.name = name;
+	}
+
+}
